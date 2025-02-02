e2501e11b3e3441e50c5b5a4f40953445ba7d3a9
NagatsukiSep <nagamatsu.r.ab@m.titech.ac.jp>
2025-02-03 02:38:27 +0900

    データベース接続設定を環境変数から取得するように変更

--- model/db.go
+++ model/db.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,7 +13,14 @@ var DB *gorm.DB
 var err error
 
 func init() {
-	dsn := "tester:password@tcp(db:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	user := os.Getenv("NS_MARIADB_USER")
+	password := os.Getenv("NS_MARIADB_PASSWORD")
+	host := os.Getenv("NS_MARIADB_HOSTNAME")
+	port := os.Getenv("NS_MARIADB_PORT")
+	database := os.Getenv("NS_MARIADB_DATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, database)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(dsn + "database can't connect")
